776b84780de64a9c2e87215dfddf8a794936a01b
HiroshiN <hinasur252@gmail.com>
2020-12-06 17:49:02 +0900

    Add adminLTE.

--- web/main/urls.py
+++ web/main/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "main"
+
 urlpatterns = [
   path('', views.index, name='index'),
   path('post/<int:pk>/', views.post_detail, name='post_detail'),
